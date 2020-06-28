d047b967773f9fcffa0c9a3c09d49d6dbdb5cb65
Shuyang Shi <shuyang790@gmail.com>
2020-06-27 23:54:26 -0400

    improve control logic

--- init.lua
+++ init.lua
@@ -29,13 +29,6 @@ hs.alert.show("Vroom: Config loaded")
 --------------------------------------------------------
 
 --------------------------------------------------------
--- Paste Password In
-hs.hotkey.bind({"cmd", "alt"}, "V", function()
-  hs.eventtap.keyStrokes(hs.pasteboard.getContents())
-end)
---------------------------------------------------------
-
---------------------------------------------------------
 -- Spotify
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", function()
   if hs.spotify.isRunning() then
@@ -54,35 +47,58 @@ end)
 
 --------------------------------------------------------
 -- Evernote notebook backup
-evernote_backup_timer = hs.timer.new(60 * 60 * 24, function()
-  hs.osascript.applescript(
-    'tell application "Evernote"' .. "\n"
-    .. '  set matches to find notes "notebook:fb-Local"' .. "\n"
-    .. '  set htmlPath to "/Volumes/GoogleDrive/My Drive/backup/fb-local-html-" & (current date)' .. "\n"
-    .. '  set enexPath to "/Volumes/GoogleDrive/My Drive/backup/fb-local-enex-" & (current date)' .. "\n"
-    .. '  export matches to htmlPath format HTML' .. "\n"
-    .. '  export matches to enexPath format ENEX' .. "\n"
-    .. 'end tell' .. "\n"
-  )
-  hs.alert.show("Evernote Local Notebook Backed up")
-end)
--- evernote_backup_timer:start()
+notebook_name = "fb-Local"
+fb_note_exe, fb_note_cnt = hs.osascript.applescript(
+  'tell application "Evernote"' .. "\n"
+  .. '  set matches to find notes "notebook:' .. notebook_name .. '"' .. "\n"
+  .. '  count matches' .. "\n"
+  .. 'end tell' .. "\n"
+)
+if fb_note_exe and fb_note_cnt > 0 then 
+  hs.alert.show("Found " .. fb_note_cnt .. " notes in " .. notebook_name .. " notebook\n Automatic backup on.")
+  evernote_backup_timer = hs.timer.new(60 * 60 * 24, function()
+    hs.osascript.applescript(
+      'tell application "Evernote"' .. "\n"
+      .. '  set matches to find notes "notebook:' .. notebook_name .. '"' .. "\n"
+      .. '  set htmlPath to "/Volumes/GoogleDrive/My Drive/backup/fb-local-html-" & (current date)' .. "\n"
+      .. '  set enexPath to "/Volumes/GoogleDrive/My Drive/backup/fb-local-enex-" & (current date)' .. "\n"
+      .. '  export matches to htmlPath format HTML' .. "\n"
+      .. '  export matches to enexPath format ENEX' .. "\n"
+      .. 'end tell' .. "\n"
+    )
+    hs.alert.show("Evernote Local Notebook Backed up")
+  end)
+  evernote_backup_timer:start()
+end
+
 
 --------------------------------------------------------
 -- Clipboard Tool (text only)
-clipboardTool = hs.loadSpoon("ClipboardTool")
-clipboardTool.paste_on_select = true
-clipboardTool.hist_size = 200
-clipboardTool:bindHotkeys({
-  show_clipboard={{"cmd", "shift"}, "V"},
-  toggle_clipboard={{"cmd", "shift"}, "V"}
-})
-clipboardTool:start()
+if hs.spoons.isInstalled("ClipboardTool") then
+  clipboardTool = hs.loadSpoon("ClipboardTool")
+  if hs.spoons.isLoaded("ClipboardTool") then 
+    clipboardTool.paste_on_select = true
+    clipboardTool.hist_size = 200
+    clipboardTool:bindHotkeys({
+      show_clipboard={{"cmd", "shift"}, "V"},
+      toggle_clipboard={{"cmd", "shift"}, "V"}
+    })
+    clipboardTool:start()
+  end
+else
+  hs.alert.show("Spoon ClipboardTool not installed!")
+end
 
 --------------------------------------------------------
 -- HSearch 
-hSearch = hs.loadSpoon("HSearch")
-hSearch:loadSources()
-hs.hotkey.bind("alt", "space", function()
-  hSearch:toggleShow()
-end)
+if hs.spoons.isInstalled("HSearch") then
+  hSearch = hs.loadSpoon("HSearch")
+  if hs.spoons.isLoaded("HSearch") then 
+    hSearch:loadSources()
+    hs.hotkey.bind("alt", "space", function()
+      hSearch:toggleShow()
+    end)
+  end
+else
+  hs.alert.show("Spoon HSearch not installed!")
+end
