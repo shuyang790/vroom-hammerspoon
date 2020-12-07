12a6a8ce18d31baf3fa98a2e2be54d5f29fec964
Shuyang Shi <shuyang790@fb.com>
2020-12-06 21:51:26 -0500

    Remove unused evernote backup

--- fbinternal.lua
+++ fbinternal.lua
@@ -1,32 +1,5 @@
 
 --------------------------------------------------------
--- Evernote notebook backup
--- notebook_name = "fb-Local"
--- fb_note_exe, fb_note_cnt = hs.osascript.applescript(
---   'tell application "Evernote"' .. "\n"
---   .. '  set matches to find notes "notebook:' .. notebook_name .. '"' .. "\n"
---   .. '  count matches' .. "\n"
---   .. 'end tell' .. "\n"
--- )
--- if fb_note_exe and fb_note_cnt > 0 then 
---   hs.alert.show("Found " .. fb_note_cnt .. " notes in " .. notebook_name .. " notebook\n Automatic backup on.")
---   evernote_backup_timer = hs.timer.new(60 * 60 * 24, function()
---     hs.osascript.applescript(
---       'tell application "Evernote"' .. "\n"
---       .. '  set matches to find notes "notebook:' .. notebook_name .. '"' .. "\n"
---       .. '  set htmlPath to "/Volumes/GoogleDrive/My Drive/backup/fb-local-html-" & (current date)' .. "\n"
---       .. '  set enexPath to "/Volumes/GoogleDrive/My Drive/backup/fb-local-enex-" & (current date)' .. "\n"
---       .. '  export matches to htmlPath format HTML' .. "\n"
---       .. '  export matches to enexPath format ENEX' .. "\n"
---       .. 'end tell' .. "\n"
---     )
---     hs.alert.show("Evernote Local Notebook Backed up")
---   end)
---   evernote_backup_timer:start()
--- end
-
-
---------------------------------------------------------
 -- VPN status
 vpn_menubar = hs.menubar.new()
 vpn_menubar:setIcon(hs.image.imageFromPath("./resources/lock.png"):setSize({w=16, h=16}))
