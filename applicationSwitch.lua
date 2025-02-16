53bd55a12b99ebd7aeb0c909aaaabc8cf419c458
Shuyang Shi <shuyangshi@mac.mynetworksettings.com>
2025-02-15 20:18:17 -0500

    update application keys; remove fbinternal import

--- applicationSwitch.lua
+++ applicationSwitch.lua
@@ -1,165 +1,155 @@
 -- Application Switch
 
-
 --------------------------------------------------------
 -- Window Hints
-hs.hotkey.bind({"ctrl", "alt"}, "E", function()
-  hs.hints.hintChars = {
-    "E", "D", "C",
-    "R", "F", "V",
-    "Y", "H", "N",
-    "U", "J", "M",
-    "A", "S",
-    "K", "L"
-  }
-  hs.hints.showTitleThresh = 10
-  hs.hints.style = "vimperator"
-  hs.hints.windowHints(
-    hs.window.filter.new()
-      :getWindows()
-  )
+hs.hotkey.bind({ "ctrl", "alt" }, "E", function()
+	hs.hints.hintChars = {
+		"E",
+		"D",
+		"C",
+		"R",
+		"F",
+		"V",
+		"Y",
+		"H",
+		"N",
+		"U",
+		"J",
+		"M",
+		"A",
+		"S",
+		"K",
+		"L",
+	}
+	hs.hints.showTitleThresh = 10
+	hs.hints.style = "vimperator"
+	hs.hints.windowHints(hs.window.filter.new():getWindows())
 end)
 --------------------------------------------------------
 
 --------------------------------------------------------
 -- Template
 function switchToAppByName(name)
-  return function()
-    local launch = hs.application.launchOrFocus(name)
-    if not launch then
-      hs.alert.show("Vroom cannot launch " .. name .. " :(")
-    end
-  end
+	return function()
+		local launch = hs.application.launchOrFocus(name)
+		if not launch then
+			hs.alert.show("Vroom cannot launch " .. name .. " :(")
+		end
+	end
 end
 
 function switchToAppByBundleID(bundleID)
-  return function()
-    local launch = hs.application.launchOrFocusByBundleID(bundleID)
-    if not launch then
-      hs.alert.show("Vroom cannot launch " .. bundleID .. " :(")
-    end
-  end
+	return function()
+		local launch = hs.application.launchOrFocusByBundleID(bundleID)
+		if not launch then
+			hs.alert.show("Vroom cannot launch " .. bundleID .. " :(")
+		end
+	end
 end
 --------------------------------------------------------
 
 -- Atom / VS Code
-hs.hotkey.bind({"ctrl", "shift"}, "A",
-  switchToAppByName("VS Code @ FB")
-)
+hs.hotkey.bind({ "ctrl", "shift" }, "A", switchToAppByName("Visual Studio Code"))
 
 -- Browser
-hs.hotkey.bind({"ctrl", "shift"}, "B",
-  switchToAppByName("Safari")
-)
+hs.hotkey.bind({ "ctrl", "shift" }, "B", switchToAppByName("Safari"))
 
 -- Fantastical / Calendar
-hs.hotkey.bind({"ctrl", "shift"}, "C",
-  -- switchToAppByName("Calendar")
-  switchToAppByName("Fantastical")
-)
+hs.hotkey.bind({ "ctrl", "shift" }, "C", switchToAppByName("Calendar"))
 
 -- Evernote / Notes
-hs.hotkey.bind({"ctrl", "shift"}, "E",
-  switchToAppByName("Notes")
-)
-
+hs.hotkey.bind({ "ctrl", "shift" }, "E", switchToAppByName("Notes"))
 
 function chrome_switch_to(ppl)
-    return function()
-        hs.application.launchOrFocus("Google Chrome")
-        local chrome = hs.appfinder.appFromName("Google Chrome")
-        local str_menu_item
-        if ppl == "Incognito" then
-            str_menu_item = {"File", "New Incognito Window"}
-        else
-            str_menu_item = {"Profiles", ppl}
-        end
-        local menu_item = chrome:findMenuItem(str_menu_item)
-        if (menu_item) then
-            chrome:selectMenuItem(str_menu_item)
-        end
-    end
+	return function()
+		hs.application.launchOrFocus("Google Chrome")
+		local chrome = hs.appfinder.appFromName("Google Chrome")
+		local str_menu_item
+		if ppl == "Incognito" then
+			str_menu_item = { "File", "New Incognito Window" }
+		else
+			str_menu_item = { "Profiles", ppl }
+		end
+		local menu_item = chrome:findMenuItem(str_menu_item)
+		if menu_item then
+			chrome:selectMenuItem(str_menu_item)
+		end
+	end
 end
 
 -- Chrome
-hs.hotkey.bind({"ctrl", "shift"}, "G",
-  chrome_switch_to("Shuyang (Personal)")
-  -- switchToAppByBundleID("com.google.chrome")
+hs.hotkey.bind(
+	{ "ctrl", "shift" },
+	"G",
+	-- chrome_switch_to("Shuyang (Personal)")
+	switchToAppByBundleID("com.google.chrome")
 )
 
 -- Firefox
-hs.hotkey.bind({"ctrl", "shift"}, "F",
-  -- switchToAppByName("Firefox Developer Edition")
-  switchToAppByBundleID("org.mozilla.firefox")
+hs.hotkey.bind(
+	{ "ctrl", "shift" },
+	"F",
+	-- switchToAppByName("Firefox Developer Edition")
+	switchToAppByBundleID("org.mozilla.firefox")
 )
 
 -- Email
-hs.hotkey.bind({"ctrl", "shift"}, "M",
-  -- switchToAppByName("Mail")
-  switchToAppByName("Spark")
+hs.hotkey.bind(
+	{ "ctrl", "shift" },
+	"M",
+	-- switchToAppByName("Mail")
+	switchToAppByName("Spark")
 )
 
--- OmniFocus
-hs.hotkey.bind({"ctrl", "shift"}, "N",
-  switchToAppByName("OmniFocus")
-)
+-- Notes
+hs.hotkey.bind({ "ctrl", "shift" }, "N", switchToAppByName("Notes.app"))
 
 -- Outlook
-hs.hotkey.bind({"ctrl", "shift"}, "O",
-  switchToAppByBundleID("com.microsoft.Outlook")
-  -- switchToAppByName("Outlook")
+hs.hotkey.bind(
+	{ "ctrl", "shift" },
+	"O",
+	switchToAppByBundleID("com.microsoft.Outlook")
+	-- switchToAppByName("Outlook")
 )
 
-hs.hotkey.bind({"ctrl", "shift"}, "P",
-  -- switchToAppByName("Microsoft To Do")
-  switchToAppByName("Music.app")
-  -- switchToAppByName("Youtube Music.app")
-)
-
--- Craft
-hs.hotkey.bind({"ctrl", "shift"}, "R",
-  switchToAppByName("Craft")
+hs.hotkey.bind(
+	{ "ctrl", "shift" },
+	"P",
+	-- switchToAppByName("Microsoft To Do")
+	switchToAppByName("Music.app")
 )
 
 -- Spotify
-hs.hotkey.bind({"ctrl", "shift"}, "S",
-  switchToAppByBundleID("com.spotify.client")
-)
+hs.hotkey.bind({ "ctrl", "shift" }, "S", switchToAppByBundleID("com.spotify.client"))
 
 -- iTerm
-hs.hotkey.bind({"ctrl", "shift"}, "T",
-  switchToAppByBundleID("com.googlecode.iterm2")
-)
+hs.hotkey.bind({ "ctrl", "shift" }, "T", switchToAppByBundleID("com.googlecode.iterm2"))
 
--- Vivaldi / Chrome FB 
-hs.hotkey.bind({"ctrl", "shift"}, "V",
-  chrome_switch_to("Shuyang (Facebook)")
-  -- switchToAppByName("Firefox Developer Edition")
-  -- switchToAppByBundleID("com.vivaldi.Vivaldi")
-)
+-- Vivaldi
+hs.hotkey.bind({ "ctrl", "shift" }, "V", switchToAppByBundleID("com.vivaldi.Vivaldi"))
 
 -- Wechat
-hs.hotkey.bind({"ctrl", "shift"}, "W",
-  switchToAppByBundleID("com.tencent.xinWechat")
+hs.hotkey.bind({ "ctrl", "shift" }, "W", switchToAppByBundleID("com.tencent.xinWechat"))
+
+hs.hotkey.bind(
+	{ "ctrl", "shift" },
+	"Y",
+	switchToAppByName("Typora")
+	-- switchToAppByName("Mark Text")
 )
 
-hs.hotkey.bind({"ctrl", "shift"}, "Y",
-  switchToAppByName("Typora")
-  -- switchToAppByName("Mark Text")
-)
+-- Things 3
+hs.hotkey.bind({ "ctrl", "shift" }, "H", switchToAppByName("Things3"))
 
 -- Zoom
-hs.hotkey.bind({"ctrl", "shift"}, "Z",
-  switchToAppByName("zoom.us")
-)
+hs.hotkey.bind({ "ctrl", "shift" }, "Z", switchToAppByName("zoom.us"))
 --------------------------------------------------------
 -- Helper
 function findApplicationBundleID(name)
-  local apps = hs.application.runningApplications()
-  for k,v in pairs(apps) do
-    if string.find(v:name(), name) ~= nil then
-      hs.alert(v:bundleID(), 10)
-    end
-  end
+	local apps = hs.application.runningApplications()
+	for k, v in pairs(apps) do
+		if string.find(v:name(), name) ~= nil then
+			hs.alert(v:bundleID(), 10)
+		end
+	end
 end
-
--- init.lua
+++ init.lua
@@ -1,31 +1,28 @@
-
-require "hs.application"
-require "windowManagement"
-require "applicationSwitch"
-require "layoutManagement"
-require "fbinternal"
+require("hs.application")
+require("windowManagement")
+require("applicationSwitch")
+require("layoutManagement")
 
 clipboardToolOn = false
 
 --------------------------------------------------------
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "A", function()
-  hs.alert.show("Hello! My name is Vroom :)")
-  hs.notify.new({title="Vroom", informativeText="Hello, my friend! :)"})
-    :send()
+hs.hotkey.bind({ "cmd", "shift", "ctrl" }, "A", function()
+	hs.alert.show("Hello! My name is Vroom :)")
+	hs.notify.new({ title = "Vroom", informativeText = "Hello, my friend! :)" }):send()
 end)
 
 --------------------------------------------------------
 -- Reload Config
 function reloadConfig(files)
-  doReload = false
-  for _, file in pairs(files) do
-    if file:sub(-4) == ".lua" then
-      doReload = true
-    end
-  end
-  if doReload then
-    hs.reload()
-  end
+	doReload = false
+	for _, file in pairs(files) do
+		if file:sub(-4) == ".lua" then
+			doReload = true
+		end
+	end
+	if doReload then
+		hs.reload()
+	end
 end
 local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Vroom: Config loaded")
@@ -33,44 +30,48 @@ hs.alert.show("Vroom: Config loaded")
 
 --------------------------------------------------------
 -- Spotify
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "S", function()
-  if hs.spotify.isRunning() then
-    hs.alert.show(
-      "Track: " .. hs.spotify.getCurrentTrack() .. "\n"
-      .. "Artist: " .. hs.spotify.getCurrentArtist() .. "\n"
-      .. "Album: " .. hs.spotify.getCurrentAlbum()
-    )
-  end
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "S", function()
+	if hs.spotify.isRunning() then
+		hs.alert.show(
+			"Track: "
+				.. hs.spotify.getCurrentTrack()
+				.. "\n"
+				.. "Artist: "
+				.. hs.spotify.getCurrentArtist()
+				.. "\n"
+				.. "Album: "
+				.. hs.spotify.getCurrentAlbum()
+		)
+	end
 end)
 --------------------------------------------------------
 
-hs.hotkey.bind({"cmd", "shift", "ctrl"}, "Q", function()
-  hs.caffeinate.lockScreen()
+hs.hotkey.bind({ "cmd", "shift", "ctrl" }, "Q", function()
+	hs.caffeinate.lockScreen()
 end)
-
 
 --------------------------------------------------------
 -- Clipboard Tool (text only)
 if clipboardToolOn and hs.spoons.isInstalled("ClipboardTool") then
-  clipboardTool = hs.loadSpoon("ClipboardTool")
-  if hs.spoons.isLoaded("ClipboardTool") then 
-    clipboardTool.paste_on_select = true
-    clipboardTool.hist_size = 200
-    clipboardTool:bindHotkeys({
-      show_clipboard={{"cmd", "shift"}, "V"},
-      toggle_clipboard={{"cmd", "shift"}, "V"}
-    })
-    clipboardTool:start()
-  end
+	clipboardTool = hs.loadSpoon("ClipboardTool")
+	if hs.spoons.isLoaded("ClipboardTool") then
+		clipboardTool.paste_on_select = true
+		clipboardTool.hist_size = 200
+		clipboardTool:bindHotkeys({
+			show_clipboard = { { "cmd", "shift" }, "V" },
+			toggle_clipboard = { { "cmd", "shift" }, "V" },
+		})
+		clipboardTool:start()
+	end
 else
-  hs.alert.show("Spoon ClipboardTool not installed!")
+	hs.alert.show("Spoon ClipboardTool not installed!")
 end
 
 --------------------------------------------------------
--- HSearch 
+-- HSearch
 -- if hs.spoons.isInstalled("HSearch") then
 --   local hSearch = hs.loadSpoon("HSearch")
---   if hs.spoons.isLoaded("HSearch") then 
+--   if hs.spoons.isLoaded("HSearch") then
 --     hSearch:loadSources()
 --     hs.hotkey.bind("alt", "space", function()
 --       hSearch:toggleShow()
