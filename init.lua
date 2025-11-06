672567da92bc8f287b60f3680b1a16938e544ce5
Shuyang Shi <shuyang790@gmail.com>
2025-11-05 20:23:52 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,16 +4,15 @@ require("modules.applicationSwitch")
 require("modules.layoutManagement")
 require("modules.clipboard")
 
-
 --------------------------------------------------------
 hs.hotkey.bind({ "cmd", "shift", "ctrl" }, "A", function()
-	hs.alert.show("Hello! My name is Vroom :)")
-	hs.notify.new({ title = "Vroom", informativeText = "Hello, my friend! :)" }):send()
+	hs.alert.show("Hello! I am your help from Hammerspoon :)")
+	hs.notify.new({ title = "Hammerspoon", informativeText = "Hello, my friend! :)" }):send()
 end)
 
 --------------------------------------------------------
 -- Reload Config
-function reloadConfig(files)
+local function reloadConfig(files)
 	local doReload = false
 	for _, file in pairs(files) do
 		if file:sub(-4) == ".lua" then
@@ -25,7 +24,7 @@ function reloadConfig(files)
 	end
 end
 local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Vroom: Config loaded")
+hs.alert.show("Hammerspoon: Config loaded")
 --------------------------------------------------------
 
 hs.hotkey.bind({ "cmd", "shift", "ctrl" }, "Q", function()
